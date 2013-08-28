89742af0c33c2b97aad1b158a4ea43a6954f1b22
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-08-27 22:37:52 -0300

    Modelo más o menos

---
+++ models/historical.rb
@@ -0,0 +1,10 @@
+class Historical < ActiveRecord::Base
+  property historial_id, :as => :integer
+  property grd_id, :as => :integer
+  property register_type, :as => :timestamp
+  property timestamp, :as => :datetime
+  property address, :as => :integer
+  property value, :as => :integer
+  property historical_type, :as => :integer
+  property insertion_time, :as => :timestamp
+end
