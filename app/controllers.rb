070fae86fe5becf8614dc4360d4ba0a7c661d3c5
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-08-27 23:15:56 -0300

    Lista completa en /all

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,23 +1,11 @@
+require 'pry'
 Dusttrak::App.controllers  do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
+  get :index do
+  end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  get :all do
+    @historical = Historical.all
 
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-
+    render 'historical/all'
+  end
 end
