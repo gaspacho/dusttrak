f1c31b9964aafc573bb5d5b233d73d9ae8216cca
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-08-28 19:39:17 -0300

    Controlador preparado para todas las vistas

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,11 +1,21 @@
 require 'pry'
 Dusttrak::App.controllers  do
   get :index do
+    render 'index/index'
   end
 
   get :all do
     @historical = Historical.all
 
     render 'historical/all'
+  end
+
+  get :grouped do
+  end
+
+  get :threshold do
+  end
+
+  get :below do
   end
 end
