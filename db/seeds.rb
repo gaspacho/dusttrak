be457763402b2ffde271b427e520d21ee407cc18
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-08-29 14:57:23 -0300

    Configuración más accesible

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,6 @@
 # Corriente en µA (I)
-corriente = Configuracion.new({ :atributo => 'corriente',
-                                :valor    => '2500' })
-
+Configuracion.create atributo: 'corriente', valor: '2500'
 # Valor de la escala (E)
-escala = Configuracion.new({ :atributo => 'escala',
-                             :valor    => '3.2' })
-
+Configuracion.create atributo: 'escala',    valor: '3.2'
 # Agrupar cada X minutos
-grouped = Configuracion.new({ :atributo => 'grouped',
-                              :valor    => '15' })
-
-escala.save
-corriente.save
-grouped.save
+Configuracion.create atributo: 'grouped',   valor: '15'
--- models/configuracion.rb
+++ models/configuracion.rb
@@ -1,6 +1,18 @@
 class Configuracion < ActiveRecord::Base
-  # Rango según el que agrupar
+  validates_uniqueness_of :atributo
+
+  # Rango de tiempo en minutos según el que agrupar
   def self.grouped
     where(atributo: 'grouped').first.valor.to_f
+  end
+
+  # Valor de E
+  def self.escala
+    where(atributo: 'escala').first.valor.to_f
+  end
+
+  # Corriente en µA (I)
+  def self.corriente
+    where(atributo: 'corriente').first.valor
   end
 end
