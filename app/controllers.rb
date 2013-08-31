07887f90a806cc997a11117bc61b987d73a691c3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-08-31 02:58:49 -0300

    DRYed

--- app/controllers.rb
+++ app/controllers.rb
@@ -4,31 +4,14 @@ Dusttrak::App.controllers  do
   end
 
   get :all do
-    @historical = Historical.mas_concentracion
-
-    # TODO DRY!
-    if not params[:desde].to_s.empty?
-      @historical = @historical.desde(params[:desde])
-    end
-
-    if not params[:hasta].to_s.empty?
-      @historical = @historical.hasta(params[:hasta])
-    end
+    @historical = filtrar(Historical.mas_concentracion)
 
     render 'historical/all'
   end
 
   # Agrupar cada 15 minutos
   get :grouped do
-    @historical = Historical.mas_concentracion.cada(Configuracion.grouped)
-
-    if not params[:desde].to_s.empty?
-      @historical = @historical.desde(params[:desde])
-    end
-
-    if not params[:hasta].to_s.empty?
-      @historical = @historical.hasta(params[:hasta])
-    end
+    @historical = filtrar(Historical.mas_concentracion.cada(Configuracion.grouped))
 
     render 'historical/all'
   end
@@ -38,4 +21,11 @@ Dusttrak::App.controllers  do
 
   get :below do
   end
+
+end
+
+def filtrar(historical)
+  historical = historical.desde(params[:desde]) if params[:desde].present?
+  historical = historical.hasta(params[:hasta]) if params[:hasta].present?
+  historical
 end
