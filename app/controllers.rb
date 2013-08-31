9e64524ded871581e6db4a645af31cfedcebb6ca
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-08-30 21:16:53 -0300

    Filtrar en las vistas

--- app/controllers.rb
+++ app/controllers.rb
@@ -6,12 +6,29 @@ Dusttrak::App.controllers  do
   get :all do
     @historical = Historical.mas_concentracion
 
+    # TODO DRY!
+    if not params[:desde].to_s.empty?
+      @historical = @historical.desde(params[:desde])
+    end
+
+    if not params[:hasta].to_s.empty?
+      @historical = @historical.hasta(params[:hasta])
+    end
+
     render 'historical/all'
   end
 
   # Agrupar cada 15 minutos
   get :grouped do
     @historical = Historical.mas_concentracion.cada(Configuracion.grouped)
+
+    if not params[:desde].to_s.empty?
+      @historical = @historical.desde(params[:desde])
+    end
+
+    if not params[:hasta].to_s.empty?
+      @historical = @historical.hasta(params[:hasta])
+    end
 
     render 'historical/all'
   end
