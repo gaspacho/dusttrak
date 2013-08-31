1861cc23fd5aa65494815e6fe6947b6a9e6e62ed
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-08-31 05:05:21 -0300

    No jugar con fuego

--- models/historical.rb
+++ models/historical.rb
@@ -1,3 +1,12 @@
+# Definición en la BD (comentado acá porque está ignorado en el esquema)
+#   primary_key "historial_id"
+#   integer     "grd_id",         :null => false
+#   integer     "register_type",  :null => false
+#   datetime    "timestamp",      :null => false
+#   integer     "address",        :null => false
+#   integer     "value",          :null => false
+#   integer     "historical_type"
+#   timestamp   "insertion_time", :null => false
 class Historical < ActiveRecord::Base
   self.table_name = 'historical'
   C = Configuracion
