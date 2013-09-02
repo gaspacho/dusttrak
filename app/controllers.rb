c0a75117cc32da727145c8d5f6a164489f6eebfa
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-09-02 19:51:18 -0300

    Por que mierda no se comiteo esto

--- app/controllers.rb
+++ app/controllers.rb
@@ -16,14 +16,8 @@ Dusttrak::App.controllers  do
     render 'historical/all'
   end
 
-  get :threshold do
+  get :above do
     @historical = filtrar(Historical.sobre_umbral)
-
-    render 'historical/all'
-  end
-
-  get :below do
-    @historical = filtrar(Historical.mas_concentracion)
 
     render 'historical/all'
   end
