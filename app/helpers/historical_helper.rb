63bcfe3a129ed15649e430ad9e47cdd971d8aaac
Nicolás Reynolds <fauno@endefensadelsl.org>
2013-09-04 20:46:07 -0300

    Más DRY y escritura de XLS, #6

--- app/helpers/historical_helper.rb
+++ app/helpers/historical_helper.rb
@@ -5,4 +5,58 @@ Dusttrak::App.helpers do
   def grd
     Historical.select('grd_id, concat(grd_id, " (", count(*), ")") as name').group('grd_id')
   end
+
+  def render_all(historical)
+    if params[:xls].present?
+      file = write_xls(@historical)
+
+      if not file == false
+        send_file file, type: 'application/vnd.ms-excel', filename: File.basename(file)
+      end
+    else
+      @historical = @historical.paginate(page: params[:page])
+      @grd = grd
+
+      render 'historical/all', :locals => {
+        :bootstrap => BootstrapPagination::Sinatra
+      }
+    end
+  end
+
+  # Escribe una planilla y devuelve el nombre de archivo
+  def write_xls(historical)
+
+    # TODO mover a filtro_to_s o algo asi para usar en todos lados
+    name = "Dusttrak"
+    name << " #{params[:grd_id]}" if params[:grd_id].present?
+    name << " #{params[:desde]}" if params[:desde].present?
+    name << " - #{params[:hasta]}" if params[:hasta].present?
+
+    file = "public/files/#{Time.now} - #{name}.xls"
+    count = historical.count
+
+    begin
+      archive = WriteExcel.new(file)
+      sheet   = archive.add_worksheet
+
+      sheet.write(0, 0, "Siafa")
+      sheet.write(1, 0, "Dusttrak")
+
+      sheet.write(3, 0, "Filtro: #{name}")
+
+      header = historical[0].attributes.keys
+      sheet.write(4, 0, header)
+
+      matriz = historical.collect { |row| row.attributes.values }
+      sheet.write(5, 0, matriz.transpose)
+
+      archive.close
+    rescue
+      return false
+    end
+
+    # Devolver el nombre de archivo
+    file
+
+  end
 end
