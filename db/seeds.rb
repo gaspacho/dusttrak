047ac10fd25611a30f5b44da0bf3350685b4d250
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-09-06 03:50:30 -0300

    Usuario con bcrypt

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 # Umbral de error default
 Configuracion.create unless Configuracion.any?
+
+if ENV['password'].present?
+  Usuario.create(nombre: 'admin', password: ENV['password'])
+else
+  puts 'Probá de nuevo con'
+  puts '  rake db:seed password="el password del admin global"'
+end
